e18706d4b7701dfd64edf069e065a8d515d3b0c1
diatlova <dari.diatlova@gmail.com>
2022-04-05 18:48:07 +0300

    debug metrics computation

--- src/model/metrics.py
+++ src/model/metrics.py
@@ -5,7 +5,7 @@ import numpy as np
 from librosa.feature import melspectrogram, mfcc
 
 
-def mel_cepstral_distance(audio1: np.ndarray, audio2: np.ndarray, sr: int = 44100, n_mfcc: int = 16,
+def mel_cepstral_distance(audio1: np.ndarray, audio2: np.ndarray, sr: int = 22050, n_mfcc: int = 16,
                           n_fft: int = 1024, hop_len: int = 256, win_len: int = 1024) -> float:
     """
     Implementation of  "Mel-Cepstral Distance Measure for Objective Speech Quality Assessment" by R. Kubichek:
@@ -15,12 +15,14 @@ def mel_cepstral_distance(audio1: np.ndarray, audio2: np.ndarray, sr: int = 4410
 
     k = 10 / np.log(10) * np.sqrt(2)
 
-    mel1 = melspectrogram(
-        y=audio1 / np.max(abs(audio1)), sr=sr, n_fft=n_fft, hop_length=hop_len, win_length=win_len, power=2
-    )
-    mel2 = melspectrogram(
-        y=audio2 / np.max(abs(audio2)), sr=sr, n_fft=n_fft, hop_length=hop_len, win_length=win_len, power=2
-    )
+    if np.max(abs(audio1)) > 1:
+        audio1 /= np.max(abs(audio1))
+
+    if np.max(abs(audio2)) > 1:
+        audio2 /= np.max(abs(audio2))
+
+    mel1 = melspectrogram(y=audio1, sr=sr, n_fft=n_fft, hop_length=hop_len, win_length=win_len, power=2)
+    mel2 = melspectrogram(y=audio2, sr=sr, n_fft=n_fft, hop_length=hop_len, win_length=win_len, power=2)
 
     # the zeroth mfcc coefficient is omitted in the paper, and dct type 2 librosa uses while computing
     # mfcc is 2 times bigger than the one is used in the paper, so:
@@ -31,7 +33,7 @@ def mel_cepstral_distance(audio1: np.ndarray, audio2: np.ndarray, sr: int = 4410
     return mcd
 
 
-def rmse_f0(audio1: np.ndarray, audio2: np.ndarray, sr: int = 44100, fmin: int = 0, fmax: int = 8000):
+def rmse_f0(audio1: np.ndarray, audio2: np.ndarray, sr: int = 22050, fmin: int = 0, fmax: int = 8000):
     pitches1 = librosa.core.piptrack(y=audio1, sr=sr, fmin=fmin, fmax=fmax)[0]
     pitches2 = librosa.core.piptrack(y=audio2, sr=sr, fmin=fmin, fmax=fmax)[0]
     return np.sqrt(np.mean((pitches1 - pitches2) ** 2))
